9c5d3a6bc3d40194974a4cb09e575e3ef8d67f75
sakib3 <sakib3@gmail.com>
2016-01-15 00:42:21 +0100

    make http req json and display in main page

--- app/catalog/catalog.js
+++ app/catalog/catalog.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module("movieDB.catalog", [])
-		.controller('catalogCtrl', ['$scope', function($scope){
-
+		.controller('catalogCtrl', ['$scope', '$http',function($scope,$http){
+			$http.get('json/movies.json').success(function(data){
+				$scope.movieList = data;
+			});
 		}])
