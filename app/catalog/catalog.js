795f8b32e2f042c2514c50bfad51049eed381f5b
sakib3 <sakib3@gmail.com>
2016-01-24 11:16:56 +0100

    add pagination

--- app/catalog/catalog.js
+++ app/catalog/catalog.js
@@ -1,7 +1,9 @@
 'use strict';
 
-angular.module("movieDB.catalog", [])
+angular.module("movieDB.catalog", ['ui.bootstrap'])
 		.controller('catalogCtrl', ['$scope', '$http',function($scope,$http){
+			$scope.currentPage =1;
+			$scope.pageSize =12;
 			$http.get('json/movies.json').success(function(data){
 				$scope.movieList = data;
 			});
