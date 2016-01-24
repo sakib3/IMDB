77d151b9afa8ec1c6f5a1fb4e026de2a64a4c711
sakib3 <sakib3@gmail.com>
2016-01-24 14:00:37 +0100

    load partial using ng-view

--- app/app.js
+++ app/app.js
@@ -1,3 +1,8 @@
 'use strict';
 
-angular.module("movieDB",['movieDB.catalog','movieDB.paginate-filter']);
+angular.module("movieDB",['ngRoute','movieDB.catalog','movieDB.paginate-filter'])
+	   .config(['$routeProvider',function($routeProvider){
+	   		$routeProvider
+	   					.when("/",{templateUrl: "catalog/catalog.html", controller: "catalogCtrl"})
+					   	.otherwise({ redirectTo: "/"});
+	   }]);
