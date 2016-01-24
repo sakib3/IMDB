7585817e1438cf5211d5b9a903f91456ad7f4308
sakib3 <sakib3@gmail.com>
2016-01-24 11:27:39 +0100

    paginate filter is created and functioning for pagination

--- app/app.js
+++ app/app.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module("movieDB",['movieDB.catalog']);
+angular.module("movieDB",['movieDB.catalog','movieDB.paginate-filter']);
---
+++ app/components/paginate/paginate-filter.js
@@ -0,0 +1,11 @@
+'use restrict';
+angular .module('movieDB.paginate-filter',[])
+		.filter('paginate', function(){
+			return function(arr,currentPage,pageSize){
+				try{
+					return arr.slice((currentPage-1)*pageSize,currentPage*pageSize);
+				}catch(err){
+					return arr;
+				}
+			};
+		});
