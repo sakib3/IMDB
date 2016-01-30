e4cc185f85bcfba771ac7d17af3b457b4c00c97c
sakib3 <sakib3@gmail.com>
2016-01-30 21:06:26 +0100

    movie detail page template updated

--- app/app.js
+++ app/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
-angular.module("movieDB",['ngRoute','movieDB.catalog','movieDB.paginate-filter'])
+angular.module("movieDB",['ngRoute','movieDB.movie','movieDB.catalog','movieDB.paginate-filter'])
 	   .config(['$routeProvider',function($routeProvider){
 	   		$routeProvider
 	   					.when("/",{templateUrl: "catalog/catalog.html", controller: "catalogCtrl"})
+	   					.when("/movie/:id",{templateUrl: "movie/movie.html", controller: "movieCtrl"})
 					   	.otherwise({ redirectTo: "/"});
 	   }]);
